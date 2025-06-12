748b787a708ad8c2adb165007784cb61f79fd598
VMike8 <vmike0808@gmail.com>
2025-06-12 11:34:28 -0500

    Terrain / AlmasInsuficientes

--- Assets/Michael/ScriptsMichael/InteractiveObject.cs
+++ Assets/Michael/ScriptsMichael/InteractiveObject.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -5,25 +6,35 @@ using UnityEngine.SceneManagement;
 public class InteractiveObject : MonoBehaviour
 {
     private SoulCollectorA soulCollector;
-    public GameObject soulMessage;
+    public GameObject interactMessage;
+    public GameObject soulIncomplete;
+    public float timeDelayMessage = 3f;
+
 
     private void Start()
     {
         soulCollector = FindFirstObjectByType<SoulCollectorA>();
-        soulMessage.SetActive(false);
+        //interactMessage.SetActive(false);
     }
 
     public void ActiveObject()
     {
-        if(soulCollector.AlmasAlmacenadas.Count>=2)
+        if (soulCollector.AlmasAlmacenadas.Count >= 2)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            soulMessage.SetActive(false);
+            interactMessage.SetActive(false);
         }
         else
         {
-            soulMessage.SetActive(true);
+            
+            StartCoroutine(AlmasInsuficientes());
         }
-        
+    }
+
+    IEnumerator AlmasInsuficientes()
+    {
+        soulIncomplete.SetActive(true);
+        yield return new WaitForSeconds(timeDelayMessage);
+        soulIncomplete.SetActive(false);
     }
 }
